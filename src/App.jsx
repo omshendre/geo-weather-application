171c4029c09bf5eddb58019233b2cf2babc575e3
omshendre <omshendre725@gmail.com>
2024-06-01 23:02:03 +0530

    update loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 // App.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import CurrentLocation from './components/CurrentLocation';
+import Loader from './Loader'; // Import the Loader component
 
 const apiKey = import.meta.env.VITE_API_KEY;
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading data (replace with actual logic to fetch weather data)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <React.Fragment>
-      <CurrentLocation apiKey ={apiKey} />
-    </React.Fragment>
+    <div className="app">
+      {loading ? (
+        <Loader /> // Display the Loader component while loading
+      ) : (
+        <CurrentLocation apiKey={apiKey} />
+      )}
+    </div>
   );
 }
 
