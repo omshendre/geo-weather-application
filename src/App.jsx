81ec10355a9516b0f28b23eca444dd2aae9df794
omshendre <omshendre725@gmail.com>
2024-06-16 23:27:10 +0530

    loader removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import CurrentLocation from './components/CurrentLocation';
-import Loader from './Loader'; // Import the Loader component
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate loading data (replace with actual logic to fetch weather data)
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="app">
-      {loading ? (
-        <Loader /> // Display the Loader component while loading
-      ) : (
         <CurrentLocation apiKey={apiKey} />
-      )}
     </div>
   );
 }
